5221b9ebd58cc1cb65214857d4457dea11834793
JJop99 <jacopo.jop@gmail.com>
2018-04-26 23:27:28 +0200

    Sistemata chiusura div; pulito il codice da qualche classe che non esiste più; fatto lo script del pulsante cripta-decripta

--- js/fromCriptToDecript.js
+++ js/fromCriptToDecript.js
@@ -1,3 +1,9 @@
-function ChangeText(){
-  document.getElementById("buttonCriptDecript").innerHTML="Decriptazione <i class='material-icons' >keyboard_arrow_left</i>"
+function ChangeText() {
+    var element = document.getElementById('buttonCriptDecript');
+
+        if (element.innerHTML === 'Criptazione<i class="material-icons">keyboard_arrow_right</i>') element.innerHTML = 'Decriptazione<i class="material-icons" >keyboard_arrow_left</i>';
+        else {
+            element.innerHTML = 'Criptazione<i class="material-icons">keyboard_arrow_right</i>';
+        }
+
 }
