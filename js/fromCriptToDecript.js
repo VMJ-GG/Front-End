609bb6c5bb1332fcb2e33a0838ce4c60e1f2c703
VincenzoGiulio <vincenzo.bosco99@gmail.com>
2018-04-28 11:13:16 +0200

    definitive alpha

--- js/fromCriptToDecript.js
+++ js/fromCriptToDecript.js
@@ -1,7 +1,7 @@
 function ChangeText() {
   var element = document.getElementById('buttonCriptDecript');
 
-  if (element.innerHTML === '<span>Decripta</span><i class="material-icons" >keyboard_arrow_left</i>')
+  if (element.innerHTML === '<span>Decripta</span><i class="material-icons">keyboard_arrow_left</i>')
     element.innerHTML = '<span>Cripta</span><i class="material-icons">keyboard_arrow_right</i>';
   else {
       element.innerHTML = '<span>Decripta</span><i class="material-icons" >keyboard_arrow_left</i>';
